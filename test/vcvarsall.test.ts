799a3e058a6759a75b516aa7d5f8018ee40add6d
Bob Brown <bobbrow@microsoft.com>
2025-05-07 11:17:58 -0700

    Add host/arch tests

--- test/vcvarsall.test.ts
+++ test/vcvarsall.test.ts
@@ -15,4 +15,30 @@ describe('vcvarsall', () => {
         assert.ok(vars['INCLUDE']);
         assert.ok(vars['VCToolsInstallDir']);
     });
+
+    it('Should get the dev environment variables from a specific host/target arch', async () => {
+        const vs = await vswhere.getVSInstallations({
+            all: true,
+            sort: true,
+            prerelease: true,
+            requires: ['Microsoft.VisualStudio.Component.VC.Tools.x86.x64']
+        });
+        assert.ok(vs.length > 0);
+        const vars = await vcvars.getVCVars(vs[0], { arch: vcvars.Architecture.x86_x64 });
+        const toolsetVersion = vars['VCToolsVersion'];
+        assert.ok(vars['Path'].toLocaleLowerCase().includes('vc\\tools\\msvc\\' + toolsetVersion + '\\bin\\hostx86\\x64'));
+    });
+
+    it('Should get the dev environment variables from a specific host/target arch', async () => {
+        const vs = await vswhere.getVSInstallations({
+            all: true,
+            sort: true,
+            prerelease: true,
+            requires: ['Microsoft.VisualStudio.Component.VC.Tools.x86.x64']
+        });
+        assert.ok(vs.length > 0);
+        const vars = await vcvars.getVCVars(vs[0], { arch: vcvars.Architecture.x64_x86 });
+        const toolsetVersion = vars['VCToolsVersion'];
+        assert.ok(vars['Path'].toLocaleLowerCase().includes('vc\\tools\\msvc\\' + toolsetVersion + '\\bin\\hostx64\\x86'));
+    });
 });
